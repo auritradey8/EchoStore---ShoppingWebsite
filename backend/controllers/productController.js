df1c87f1e52949f6a620dedd4e9082f9696589c5
Bittukumar2003 <bittukumar06102003@gmail.com>
2024-08-24 12:03:21 +0530

    searching,filtering and pagination added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
-const catchAsyncErrors= require("../middleware/catchAsyncError")
+const catchAsyncErrors= require("../middleware/catchAsyncError");
+const ApiFeatures = require("../utils/apifeatures");
 
 // create Product --Admin
 exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
@@ -12,11 +13,18 @@ exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 });
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req,res)=>{
-    const products = await Product.find();
+    const resultPerPage=5;
+    const productCount =await Product.countDocuments();
+    
+    const apiFeature=new ApiFeatures(Product.find(),req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+    const products = await apiFeature.query;
 
     res.status(200).json({
         success:true,
-        products
+        products,
     })
 
 });
@@ -65,6 +73,7 @@ exports.getProductDetails = catchAsyncErrors(async(req,res,next)=>
     }
     res.status(200).json({
         success:true,
-        product
+        product,
+        productCount,
     })
 });
